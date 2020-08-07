2411e057efad058e0d1a6fc6c077e70c10c1c5d7
white-cat-dev <alexandra.19.98.study@gmail.com>
2020-08-07 10:51:35 +0800

    added stocks to product page

--- tct-manager.php
+++ tct-manager.php
@@ -9,5 +9,5 @@ require __DIR__ . '/functions.php';
 
 add_action('wp_enqueue_scripts', 'tct_manager_enqueue_scripts');
 // add_filter('page_template', 'tct_page_template');
-add_action('wp_insert_post_data', 'tct_manager_insert_post_data');
+// add_action('wp_insert_post_data', 'tct_manager_insert_post_data');
 add_filter('the_content', 'tct_manager_content');
